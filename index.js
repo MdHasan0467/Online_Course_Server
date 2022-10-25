1dd885e29745c36dd002f6206918bf27d3de02c1
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-10-26 00:35:31 +0600

    recheck server and include datas

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 
 const Port = process.env.Port || 5000;
 
+const courses = require('./data.json');
+
+app.get('/courses', (req, res) => {
+	res.send(courses);
+});
+app.listen(Port, () => {
+	console.log('server is running on port', Port);
+});
