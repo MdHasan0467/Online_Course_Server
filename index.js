08ef02dbeb82c6e19b04e113eb39d3aad06c0f38
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-10-26 00:33:21 +0600

    data added

--- index.js
+++ index.js
@@ -5,11 +5,3 @@ app.use(cors());
 
 const Port = process.env.Port || 5000;
 
-const courses = require('./data.json');
-
-app.get('/courses', (req, res) => {
-	res.send(courses);
-});
-app.listen(Port, () => {
-	console.log('server is running on port', Port);
-});
