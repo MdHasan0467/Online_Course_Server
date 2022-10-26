f978ad56d747510394d39af561958d03629c6b26
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-10-27 05:44:10 +0600

    vercel setup

--- index.js
+++ index.js
@@ -3,13 +3,22 @@ const app = express();
 const cors = require('cors');
 app.use(cors());
 
-const Port = process.env.Port || 5000;
+const port = process.env.Port || 5000;
 
 const courses = require('./data.json');
+
+app.get('/', (req, res) => {
+	res.send('server is running');
+});
 
 app.get('/courses', (req, res) => {
 	res.send(courses);
 });
-app.listen(Port, () => {
-	console.log('server is running on port', Port);
+app.get('/course/:id', (req, res) => {
+	const id = req.params.id;
+	const course = courses.find((p) => p.id == id);
+	res.send(course);
+});
+app.listen(port, () => {
+	console.log(`server is running on port ${port}`);
 });
